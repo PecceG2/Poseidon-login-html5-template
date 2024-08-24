b9b248a0960fd90e9a008e94e06b8864ce5353c0
Giuliano Peccetto <me@pecceg2.com>
2024-08-24 00:59:53 -0300

    + Main version

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
-const container = document.getElementById('container');
-const registerBtn = document.getElementById('register');
-const loginBtn = document.getElementById('login');
-
-registerBtn.addEventListener('click', () => {
-    container.classList.add("active");
-});
-
-loginBtn.addEventListener('click', () => {
-    container.classList.remove("active");
-});
+window.onload = function() {
+    setTimeout(function() {
+        document.getElementById('container').classList.remove('active');
+    }, 1000);
+};
